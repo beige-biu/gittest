eca47d0f5b69e396c38a87982277d58b64bb1479
beige-biu <wenyu0o@163.com>
2020-06-11 11:01:42 +0800

    three commit

--- employeemanger/src/main/java/com/wenyu/manger/EmployeemangerApplication.java
+++ employeemanger/src/main/java/com/wenyu/manger/EmployeemangerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EmployeemangerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeemangerApplication.class, args);
+		int i=10;
     }
 
 }
