5a896ba37941f8aae5bf3fd8c3e14076d3d9195c
beige-biu <wenyu0o@163.com>
2020-06-11 10:38:20 +0800

    init

---
+++ employeemanger/src/main/java/com/wenyu/manger/EmployeemangerApplication.java
@@ -0,0 +1,13 @@
+package com.wenyu.manger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeemangerApplication.class, args);
+    }
+
+}
