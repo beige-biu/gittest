2c962aa1a4f954778a7f1b0796bce6e5f216d216
beige-biu <wenyu0o@163.com>
2020-06-11 14:00:40 +0800

    wy

--- employeemanger/src/main/java/com/wenyu/manger/EmployeemangerApplication.java
+++ employeemanger/src/main/java/com/wenyu/manger/EmployeemangerApplication.java
@@ -4,17 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEurekaClient
+
 public class EmployeemangerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeemangerApplication.class, args);
-
-		int i= 20;
-		
-
-		int j= 2;
-
     }
 
 }
